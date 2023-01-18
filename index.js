126c1d10e24d7d7d2be8ab7c55fd79cf4fbaadcf
Lucas <lucasfreiresabino@gmail.com>
2023-01-18 12:53:39 -0700

    complete js code

--- index.js
+++ index.js
@@ -9,3 +9,22 @@
 // Example
 // alphabetPosition("The sunset sets at twelve o' clock.")
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+function alphabetPosition(text) {
+    let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let result = ""
+    for(let i = 0; i < text.length; i++){
+        if(text[i] !== " "){
+            if(i === (text.length - 1)){
+                result += `${alphabet.indexOf(text[i].toUpperCase()) + 1}`
+            } else {
+                result += `${alphabet.indexOf(text[i].toUpperCase()) + 1} `
+            }
+        }
+    }
+
+    console.log(result)
+  }
+
+  alphabetPosition("The sunset sets at twelve o' clock.")
